706ba450ab6b886b55f69e9585b9a6afb1424960
evronic178 <evronic178@gmail.com>
2025-04-10 16:59:04 +0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,39 +1,18 @@
-WITH settings AS (
-  SELECT
-    -- Извлекаем значения в байтах
-    (SELECT setting::bigint * CASE unit
-        WHEN 'kB' THEN 1024
-        WHEN 'MB' THEN 1024*1024
-        WHEN 'GB' THEN 1024*1024*1024
-        WHEN '8kB' THEN 8192
-        ELSE 1 END
-     FROM pg_settings WHERE name = 'shared_buffers') AS shared_buffers_bytes,
-
-    (SELECT setting::bigint * CASE unit
-        WHEN 'kB' THEN 1024
-        WHEN 'MB' THEN 1024*1024
-        WHEN 'GB' THEN 1024*1024*1024
-        ELSE 1 END
-     FROM pg_settings WHERE name = 'work_mem') AS work_mem_bytes,
-
-    (SELECT setting::bigint * CASE unit
-        WHEN 'kB' THEN 1024
-        WHEN 'MB' THEN 1024*1024
-        WHEN 'GB' THEN 1024*1024*1024
-        ELSE 1 END
-     FROM pg_settings WHERE name = 'maintenance_work_mem') AS maintenance_work_mem_bytes,
-
-    (SELECT setting::int FROM pg_settings WHERE name = 'max_connections') AS max_connections
-)
-
-SELECT
-  pg_size_pretty(shared_buffers_bytes) AS shared_buffers,
-  pg_size_pretty(work_mem_bytes) AS work_mem_per_op,
-  pg_size_pretty(maintenance_work_mem_bytes) AS maintenance_work_mem_per_session,
-  max_connections,
+explain analyze
+SELECT COUNT(*) FROM (SELECT DISTINCT RES.ID_ 
+       
   
-  -- Примерная оценка: shared_buffers + work_mem * 2 * max_connections
-  pg_size_pretty(
-    shared_buffers_bytes + (work_mem_bytes * max_connections * 2)
-  ) AS estimated_total_memory_usage
-FROM settings;
+      from ACT_ID_USER RES 
+       
+        inner join ACT_ID_MEMBERSHIP M on RES.ID_ = M.USER_ID_
+        inner join ACT_ID_GROUP G on M.GROUP_ID_ = G.ID_
+       
+       
+      
+       
+           
+      
+      
+       WHERE  G.ID_ = camunda-admin
+     
+      ) countDistinct
